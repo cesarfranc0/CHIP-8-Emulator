befc65d624037a50abf2842febcea83d58bdc361
Cesar Franco <74111769+cesarfranc0@users.noreply.github.com>
2024-07-13 02:34:53 -0400

    Add files via upload

--- include/chip8.h
+++ include/chip8.h
@@ -6,6 +6,8 @@
 #include "chip8registers.h"
 #include "chip8stack.h"
 #include "chip8keyboard.h"
+#include "chip8screen.h"
+#include <stddef.h>
 
 struct chip8
 {
@@ -13,6 +15,11 @@ struct chip8
     struct chip8_stack stack;
     struct chip8_registers registers;
     struct chip8_keyboard keyboard;
+    struct chip8_screen screen;
 };
+
+void chip8_init(struct chip8 *chip8);
+void chip8_load(struct chip8 *chip8, const char *buffer, size_t size);
+void chip8_exec(struct chip8 *chip8, unsigned short opcode);
 
 #endif
--- include/chip8registers.h
+++ include/chip8registers.h
@@ -5,12 +5,12 @@
 
 struct chip8_registers
 {
-    unsigned char V[CHIP8_TOTAL_DATA_REGISTERS];
-    unsigned short I; // register to store memory addresses
+    unsigned char V[CHIP8_TOTAL_DATA_REGISTERS]; // general purpose data registers
+    unsigned short I;                            // register to store memory addresses
     unsigned char delay_timer;
     unsigned char sound_timer;
-    unsigned short program_counter; // register that uses a pointer to point to a specified instruction set
-    unsigned char stack_pointer;
+    unsigned short program_counter; // register that uses a pointer to a currently executing address in the program's memory which is then incremented to proceed to the next instruction
+    unsigned char stack_pointer;    // points to the top of the stack which returns addresses when subroutines are called
 };
 
 #endif
